1fbbc39cb7a7d2c333a1c2069a0900485f8abd69
t-zouchi <mojamoja.000@gmail.com>
2017-08-26 18:49:50 +0900

    add music

---
+++ Assets/Scripts/MainSoundScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSoundScript : MonoBehaviour {
+  public bool DontDestroyEnabled = true;
+  // Use this for initialization
+  void Start () {
+    if (DontDestroyEnabled)
+    {
+      // Sceneを遷移してもオブジェクトが消えないようにする
+      DontDestroyOnLoad(this);
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
